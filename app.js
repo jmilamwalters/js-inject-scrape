01f276be2015c372a19f9ae4b1d72eddb248aa77
J. Milam Walters <jmilamwalters@gmail.com>
2017-07-24 16:41:03 -0500

    Implement proof-of-concept CORS HTTP request; parse, render, and append button

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors')
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(cors());
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/button', (req, res) => {
+  res.json({
+    title: 'Finance now!',
+    url: 'http://localhost/finance-redirect',
+  });
+});
+
+app.get('/cors', (req, res) => {
+  res.send('CORS requests now enabled for all origins!');
+});
+
+app.get('/finance-redirect', (req, res) => {
+  res.send('Successfully redirected!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
